d7aab4522ea39fb496aa95681ab55fa8df9222fa
yangyi <yangyi.cn.gz@gmail.com>
2015-08-31 14:58:15 +0800

    bugfix, json类型参数检查，自动补齐"keys"规则

--- src/Parameter/Checker.php
+++ src/Parameter/Checker.php
@@ -242,6 +242,10 @@ class Checker {
             throw $this->exception($key, 'json_decode() failed, '. $error);
         }
 
+        if (!isset($option['keys']) && !isset($option['element'])) {
+            $option['keys'] = [];
+        }
+
         return $this->checkArray($key, $value, $option);
     }
 
