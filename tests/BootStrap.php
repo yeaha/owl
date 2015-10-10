1f85317f0f897b170b1e1c16b6cd3dcc08ae7afa
yangyi <yangyi.cn.gz@gmail.com>
2015-10-10 12:42:52 +0800

    改进单元测试初始化脚本

--- tests/BootStrap.php
+++ tests/BootStrap.php
@@ -1,6 +1,10 @@
 <?php
+if (!file_exists(__DIR__.'/../vendor')) {
+    die('run "composer install" first'.PHP_EOL);
+}
+
 defined('TEST') or define('TEST', true);
 
-require __DIR__ .'/../src/autoload.php';
+require __DIR__ .'/../vendor/autoload.php';
 
 \Owl\Application::registerNamespace('\\Tests', __DIR__);
