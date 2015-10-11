c7ae304519de56f13bbdc99f2ce25de69e30aca1
yangyi <yangyi.cn.gz@gmail.com>
2015-10-11 14:29:07 +0800

    实现Request::getParsedBody()方法

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -166,7 +166,24 @@ class Request implements ServerRequestInterface {
     }
 
     public function getParsedBody() {
-        throw new \Exception('Request::getParsedBody() not implemented');
+        $content_type = $this->getHeaderLine('content-type');
+        $method = $this->getServerParam('REQUEST_METHOD');
+
+        if ($method === 'POST' && ($content_type === 'application/x-www-form-urlencoded' || $content_type === 'multipart/form-data')) {
+            return $this->post;
+        }
+
+        $body = (string)$this->body;
+
+        if ($body === '') {
+            return null;
+        }
+
+        if ($content_type === 'application/json') {
+            return json_decode($body, true);
+        }
+
+        return $body;
     }
 
     public function withParsedBody($data) {
