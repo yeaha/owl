af75e2e6fcdf18b7c493bbc390e58d9997009fc7
yangyi <yangyi.cn.gz@gmail.com>
2015-10-15 15:27:20 +0800

    调整常量初始化方式

--- src/constant.php
+++ src/constant.php
@@ -1,7 +1,3 @@
 <?php
 defined('DEBUG') or define('DEBUG', false);
 defined('TEST') or define('TEST', false);
-
-require __DIR__.'/Application.php';
-
-\Owl\Application::registerNamespace('\Owl', __DIR__);
