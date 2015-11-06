c0398b32ff2c3c3b5edfdb22c27a2373d353564f
yangyi <yangyi.cn.gz@gmail.com>
2015-11-06 10:29:41 +0800

    Data string type增加"allow_tags"

--- src/DataMapper/Type/String.php
+++ src/DataMapper/Type/String.php
@@ -2,7 +2,19 @@
 namespace Owl\DataMapper\Type;
 
 class String extends \Owl\DataMapper\Type\Mixed {
+    public function normalizeAttribute(array $attribute) {
+        return array_merge([
+            'allow_tags' => false,
+        ], $attribute);
+    }
+
     public function normalize($value, array $attribute) {
         return $this->isNull($value) ? null : (string)$value;
+    }
+
+    public function validateValue($value, array $attribute) {
+        if (!$this->isNull($value) && !$attribute['allow_tags'] && (strip_tags($value) != $value)) {
+            throw new \UnexpectedValueException('cannot contain tags');
+        }
     }
 }
