57f1ef119d5cbe66c3815e358a2ef4db1f0df300
yangyi <yangyi.cn.gz@gmail.com>
2015-11-14 10:36:07 +0800

    删除example

--- examples/hello_world/View/Index.php
+++
@@ -1,5 +0,0 @@
-<?php $this->extendView('_layout'); ?>
-
-<?php $this->beginBlock('main'); ?>
-<?php echo $output; ?>
-<?php $this->endBlock(); ?>
--- examples/hello_world/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('SITE_MODE', true);
-
-require __DIR__.'/boot.php';
-
-$app = __get_fpm_app();
-$app->start();
