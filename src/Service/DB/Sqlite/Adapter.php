2c9635a3f33168aa2a0864fee01edec8b436bc92
yangyi <yangyi.cn.gz@gmail.com>
2015-11-19 11:16:54 +0800

    增加Sqlite adapter

---
+++ src/Service/DB/Sqlite/Adapter.php
@@ -0,0 +1,14 @@
+<?php
+namespace Owl\Service\DB\Sqlite;
+
+if (!extension_loaded('pdo_sqlite')) {
+    throw new \Exception('Require "pdo_sqlite" extension.');
+}
+
+class Adapter extends \Owl\Service\DB\Adapter {
+    protected $identifier_symbol = '`';
+
+    public function lastID($table = null, $column = null) {
+        return $this->execute('SELECT last_insert_rowid()')->getCol();
+    }
+}
