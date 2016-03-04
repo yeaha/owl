e4db9695b073edb8c0f629390302ca717e83ba8a
yangyi <yangyi.cn.gz@gmail.com>
2016-03-04 19:36:23 +0800

    改进UUID生成函数，在php7中使用random_int()代替mt_rand()

--- src/DataMapper/Type/UUID.php
+++ src/DataMapper/Type/UUID.php
@@ -30,11 +30,20 @@ class UUID extends Common {
 
     // http://php.net/manual/en/function.uniqid.php#94959
     static public function generate() {
-        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-            mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-        );
+        if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+            return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+                mt_rand(0, 0x0fff) | 0x4000,
+                mt_rand(0, 0x3fff) | 0x8000,
+                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+            );
+        } else {
+            return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                random_int(0, 0xffff), random_int(0, 0xffff), random_int(0, 0xffff),
+                random_int(0, 0x0fff) | 0x4000,
+                random_int(0, 0x3fff) | 0x8000,
+                random_int(0, 0xffff), random_int(0, 0xffff), random_int(0, 0xffff)
+            );
+        }
     }
 }
