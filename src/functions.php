e774e9c522bcb6bcfce2eb5384752501131f20fe
yangyi <yangyi.cn.gz@gmail.com>
2016-03-30 17:06:32 +0800

    增加\Owl\Service\get()快捷函数

--- src/functions.php
+++ src/functions.php
@@ -157,3 +157,14 @@ namespace Owl
         return $value;
     }
 }
+
+namespace Owl\Service
+{
+    function get($id)
+    {
+        $args = func_get_args();
+        $container = \Owl\Service\Container::getInstance();
+
+        return call_user_func_array([$container, 'get'], $args);
+    }
+}
