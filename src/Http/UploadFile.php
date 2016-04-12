0a46cf3dd065070cee5c7544844caff3b55c5a0a
yangyi <yangyi.cn.gz@gmail.com>
2016-04-12 18:51:09 +0800

    bugfix，类名拼写错误

--- src/Http/UploadFile.php
+++ src/Http/UploadFile.php
@@ -2,7 +2,7 @@
 
 namespace Owl\Http;
 
-class UploadFile extends \Psr\Http\Message\UploadFileInterface
+class UploadFile extends \Psr\Http\Message\UploadedFileInterface
 {
     protected $moved;
     protected $file;
